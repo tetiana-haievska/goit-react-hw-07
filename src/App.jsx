d141cd0e62aa72d6b1c10cf8396e05be232d1599
Tetiana <tatiana.makarenko94@gmail.com>
2025-03-03 20:57:37 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ import { fetchContacts } from "./redux/contactsOps";
 import { selectLoading, selectError } from "./redux/contactsSlice";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
-import Filter from "./components/Filter/Filter";
+import Filter from "./components/Filter/Filter.jsx";
 import Loader from "./components/Loader/Loader";
 import styles from "./App.module.css";
 
